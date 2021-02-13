ecf82ce900e59f4f71e51d5e4e1c665901434dba
OUDRHIRI Oussama <oudrhiriouss@gmail.com>
2021-02-13 16:40:42 +0100

    first commit

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+  produits: Produit[]; 
+  constructor(private produitService: ProduitService) { 
+    this.produits = produitService.listeProduits();
+  }
+  ngOnInit(): void {
+  }
+  supprimerProduit(p:Produit){
+    let conf = confirm("Etes-vous sur?");
+    if (conf)
+      this.produitService.supprimerProduit(p)    
+  }
+}
